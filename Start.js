9645c968aea64b7f1ff076af9fea21f631c6520e
Ernest Mgłosiek <mglernest@gmail.com>
2023-07-04 20:47:48 +0200

    create search function

--- Start.js
+++ Start.js
@@ -6,14 +6,17 @@ class Start {
         this.searchInput = document.querySelector(searchInput);
         this.addBtn = document.querySelector(addBtn);
         this.tasksList = document.querySelector(tasksList);
-        this.dataTasks = [];;
+        this.dataTasks = [];
         this.addBtn.addEventListener("click", e => SetData.addTasks(e,this.addInput,this.dataTasks,this.tasksList));
         this.searchInput.addEventListener('input', this.searchItem.bind(this));
     }
     searchItem(){
         if(this.dataTasks.length == 0) return window.alert('BRAK WARTOŚCI W TABLICY');
-        const inputValue = this.value;
+        const looksWord = this.searchInput.value;
         const filterToDo = this.dataTasks.filter(item => item.textContent.toLowerCase().includes(looksWord));
+        console.log(filterToDo);
+        const render = new Render();
+        this.searchInput.value === '' ? render.RenderDataSet(this.dataTasks,this.tasksList) : render.RenderDataSet(filterToDo,this.tasksList);
     }
 
 }
